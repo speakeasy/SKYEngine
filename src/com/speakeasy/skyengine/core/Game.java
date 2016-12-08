52f0a7ef240181dfe1d6aba15fbea7e88687f96f
SpeaKeasY <speakeasysky@gmail.com>
2016-12-08 06:05:33 +0000

    taking a break. In the middle of GL 3 LWJGL 3 updates.

--- src/com/speakeasy/skyengine/core/Game.java
+++ src/com/speakeasy/skyengine/core/Game.java
@@ -1,3 +1,5 @@
+package com.speakeasy.skyengine.core;
+
 /*
  * Copyright 2016 Kevin Owen Burress <speakeasysky@gmail.com>.
  *
@@ -13,43 +15,156 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.speakeasy.skyengine.core;
+ /*package com.speakeasy.skyengine.core;
 
 import com.speakeasy.skyengine.core.timer.GameTimer;
 import com.speakeasy.skyengine.entity.player.Camera;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 /**
  *
  * @author SpeaKeasY
  */
-public class Game extends Thread {
+ /*public class Game extends Thread {
 
     protected static GameTimer gametimer;
     public static Game game;
     protected static Camera camera;
-    
-    private final static int width = 800;
-    private final static int height = 600;
-    private final static int FRAME_RATE = 60;
+
+    private static int WINDOW_WIDTH = 800;
+    private static int WINDOW_HEIGHT = 600;
+    private static int FRAME_RATE = 60;
 
     public Game(String title) {
-        try {
-            Display.setDisplayMode(new DisplayMode(width,height));
-            Display.create();
-        } catch (LWJGLException e) {
-            System.out.println(e);
+        if (glfwInit() != true) {
+            System.err.println("Error initializing GLFW");
+            System.exit(1);
         }
-        Display.setTitle(title);
-        Game game = new Game();
-        game.init();
-        int delta = 0;
-        
+
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        glfwWindowHint(GLFW_SAMPLES, 4);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+        long windowID = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Terrain App - LWJGL3", NULL, NULL);
+
+        if (windowID == NULL) {
+            System.err.println("Error creating a window");
+            System.exit(1);
+        }
+        game = this;
         gametimer = GameTimer.newGameTimer(true);
         camera = new Camera();
         Camera.init();
     }
 
+}
+ */
+import com.speakeasy.skyengine.core.timer.GameTimer;
+import com.speakeasy.skyengine.entity.player.Camera;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+/**
+ * @author Sri Harsha Chilakapati
+ */
+public class Game {
+
+    private long windowID;
+    public static Game game;
+
+    protected static GameTimer gametimer;
+    protected static Camera camera;
+
+    public static int WINDOW_WIDTH = 800;
+    public static int WINDOW_HEIGHT = 600;
+    private static int FRAME_RATE = 60;
+
+    public Game() {
+        if (!glfwInit()) {
+            System.err.println("Error initializing GLFW");
+            System.exit(1);
+        }
+
+        // Window Hints for OpenGL context
+        glfwWindowHint(GLFW_SAMPLES, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+        windowID = glfwCreateWindow(640, 480, "Terrain App - Test LWJGL3", NULL, NULL);
+
+        if (windowID == NULL) {
+            System.err.println("Error creating a window");
+            System.exit(1);
+        }
+
+        glfwMakeContextCurrent(windowID);
+        GL.createCapabilities();
+
+        glfwSwapInterval(1);
+    }
+
+    public void init() {
+        gametimer = GameTimer.newGameTimer(true);
+        camera = new Camera(windowID);
+        camera.init();
+    }
+
+    public void update(float delta) {
+    }
+
+    public void render(float delta) {
+    }
+
+    public void dispose() {
+    }
+
+    public void start() {
+        float now, last, delta;
+
+        last = 0;
+
+        // Initialise the Game
+        init();
+
+        // Loop continuously and render and update
+        while (!glfwWindowShouldClose(windowID)) {
+            // Get the time
+            now = (float) glfwGetTime();
+            delta = now - last;
+            last = now;
+
+            // Update and render
+            update(delta);
+            render(delta);
+
+            // Poll the events and swap the buffers
+            glfwPollEvents();
+            glfwSwapBuffers(windowID);
+        }
+
+        // Dispose the game
+        dispose();
+
+        // Destroy the window
+        glfwDestroyWindow(windowID);
+        glfwTerminate();
+
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        game = new Game();
+        game.start();
+    }
 }
--- src/com/speakeasy/skyengine/entity/player/Camera.java
+++ src/com/speakeasy/skyengine/entity/player/Camera.java
@@ -1,53 +1,67 @@
 package com.speakeasy.skyengine.entity.player;
 
+import com.speakeasy.skyengine.core.Game;
 import com.speakeasy.skyengine.utils.math.Vector3;
 import java.util.Observable;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CURSOR_DISABLED;
+import static org.lwjgl.glfw.GLFW.glfwSetCursorPos;
+import static org.lwjgl.glfw.GLFW.glfwSetInputMode;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
 //import net.java.games.input.Controller;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
 public class Camera extends Observable {
 
-    private static Vector3 position = new Vector3();
-    private static Vector3 oldPosition = new Vector3();
+    private Vector3 position = new Vector3();
+    private Vector3 oldPosition = new Vector3();
 
-    private static Vector3 rotation = new Vector3();
-    private static Vector3 oldRotation = new Vector3();
+    private Vector3 rotation = new Vector3();
+    private Vector3 oldRotation = new Vector3();
 
-    private static final float speed = 0.05f;
-    //private static Controller controller;
-    private static Vector3[] positionPS = new Vector3[2];
+    private final float speed = 0.05f;
+    //private Controller controller;
+    private Vector3[] positionPS = new Vector3[2];
 
-    public static float getCamX() {
+    private GLFWKeyCallback keyCallback;
+    private GLFWErrorCallback errorCallback;
+    
+    private long windowID;
+    
+    public Camera(long windowID) {
+        this.windowID = windowID;
+    }
+
+    public float getCamX() {
         return position.getX();
     }
 
-    public static float getCamY() {
+    public float getCamY() {
         return position.getY();
     }
 
-    public static float getCamZ() {
+    public float getCamZ() {
         return position.getZ() + 2;
     }
 
-    public static float getDistance(float x, float y, float z) {
+    public float getDistance(float x, float y, float z) {
         return new Vector3(x - position.getX(), y - position.getY(), z - position.getZ()).getMagnitude();
     }
 
-    public static float getFacingHoriz() {
+    public float getFacingHoriz() {
         return (270 + rotation.getZ()) % 360;
     }
 
-    public static float getRelativeDirectionHoriz(int x, int y) {
+    public float getRelativeDirectionHoriz(int x, int y) {
         return (float) (getFacingHoriz() - Math.atan2(position.getY() - y, position.getX() - x));
     }
 
-    public static float getFacingVert() {
+    public float getFacingVert() {
         return -rotation.getX();
     }
 
-    public static void init() {
+    public void init() {
         /*if (ControllerEnvironment.getDefaultEnvironment().getControllers().length > 0) {
          for (Controller controller : ControllerEnvironment.getDefaultEnvironment().getControllers()) {
          if (controller.getType() == Controller.Type.GAMEPAD) {
@@ -59,28 +73,29 @@ public class Camera extends Observable {
 
         positionPS[0] = position;
         positionPS[1] = rotation;
-        Mouse.setGrabbed(true);
+        glfwSetInputMode(windowID , GLFW_CURSOR, GLFW_CURSOR_DISABLED);
         //}
     }
 
-    public static boolean hasNotMoved() {
+    public boolean hasNotMoved() {
         return oldPosition.equals(position) && rotation.equals(oldRotation);
     }
 
-    public static Vector3[] getPositionRotation() {
+    public Vector3[] getPositionRotation() {
         return positionPS;
     }
 
-    public static String update(int delta) {
+    public String update(int delta) {
         oldPosition.set(position);
         oldRotation.set(rotation);
         updateRotation(delta);
         updatePosition(delta);
         updatePerspective();
+        glfwSetCursorPos(windowID, Game.WINDOW_WIDTH * 0.5f, Game.WINDOW_HEIGHT * 0.5f);
         return position.toString();
     }
 
-    public static void updatePosition(int delta) {
+    public void updatePosition(int delta) {
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
             position.setX(position.getX() - (float) (Math.sin(-rotation.getZ() * Math.PI / 180) * speed * delta));
             position.setY(position.getY() - (float) (Math.cos(-rotation.getZ() * Math.PI / 180) * speed * delta));
@@ -105,7 +120,7 @@ public class Camera extends Observable {
         }
     }
 
-    public static void updateRotation(int delta) {
+    public void updateRotation(int delta) {
         //Mouse Input for looking around...
         if (Mouse.isGrabbed()) {
             float mouseDX = Mouse.getDX() * 0.128f;
@@ -129,7 +144,7 @@ public class Camera extends Observable {
         }
     }
 
-    public static void updatePerspective() {
+    public void updatePerspective() {
         GL11.glRotatef(rotation.getX(), 1, 0, 0);
         GL11.glRotatef(rotation.getY(), 0, 0, 1);
         GL11.glRotatef(rotation.getZ(), 0, 1, 0);
